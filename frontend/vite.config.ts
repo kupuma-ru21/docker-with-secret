b422218b0c3730d143ec2ce103501178facd72a1
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-05-02 03:50:18 -0700

    fix: add allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["frontend-445746516167.us-central1.run.app"],
+  },
+});
